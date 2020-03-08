04ebc1ad67b5f37c847253a707be15f0ac576a3c
Satesh <janki.chaudhari@isoftbet.com>
2020-03-08 22:33:54 +0000

    Automate Register test

--- src/Automation/NewRegister.java
+++ src/Automation/NewRegister.java
@@ -6,18 +6,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import static java.lang.System.out;
-import static java.lang.System.setProperty;
+
 
 
 public class NewRegister {
 
     public static void main(String[] args) throws InterruptedException {
 
-        setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromeDriver_win32\\chromeDriver.exe");
+      //  setProperty("webdriver.chrome.driver", "satesh$ \\Users\\satesh\\Downloads\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://www.nopcommerce.com/");
+
         Thread.sleep(1000);
 
         driver.findElement(By.cssSelector("span.ico-user.sprite-image")).click();
@@ -30,45 +31,42 @@ public class NewRegister {
         if (label.equals("Register")) {
             driver.findElement(By.id("FirstName")).sendKeys("John");
             driver.findElement(By.name("LastName")).sendKeys("Smith");
-            driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("Johnsmith623@gmail.com");
-            driver.findElement(By.cssSelector("input#ConfirmEmail")).sendKeys("Johnsmith623@gmail.com");
-            driver.findElementById("Username").sendKeys("smith472fce");
+            driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("Johnsmith7123@gmail.com");
+            driver.findElement(By.cssSelector("input#ConfirmEmail")).sendKeys("Johnsmith7123@gmail.com");
+            driver.findElementById("Username").sendKeys("smith472fae");
             driver.findElementById("check-availability-button").click();
-        } else {
-            out.println("Condition not Match");
+
+
+            Thread.sleep(2000);
+
+            String checkAva = driver.findElement(By.id("username-availabilty")).getText();
+            String expectAva = "Username available";
+
+
+            if (expectAva.equals(checkAva)) {
+                out.println(expectAva);
+
+                Select Country = new Select(driver.findElementById("CountryId"));
+                Country.selectByVisibleText("United Kingdom");
+                Select Companyroll = new Select(driver.findElementById("CompanyRoleId"));
+                Companyroll.selectByVisibleText("Technical developer");
+                driver.findElement(By.className("custom-control-label")).click();
+                boolean newsLetter = driver.findElement(By.className("custom-control-label")).isSelected();
+                out.println(newsLetter);
+                Select Timezone = new Select(driver.findElementById("TimeZoneId"));
+                Timezone.selectByValue("GMT Standard Time");
+                driver.findElementById("Password").sendKeys("Password");
+                driver.findElementById("ConfirmPassword").sendKeys("Password");
+
+                WebElement Submit = driver.findElementById("register-button");
+                Submit.click();
+
+                Thread.sleep(1000);
+
+                String registration = driver.findElement(By.xpath("//div[@class='bar-notification success']/p")).getText();
+                if (registration.equals("Your registration completed")) out.println("Test Case Passed");
+            } else out.println("Test Case fail");
         }
-
-        Thread.sleep(2000);
-
-        String checkAva = driver.findElement(By.id("username-availabilty")).getText();
-        String expectAva = "Username available";
-
-
-        if (expectAva.equals(checkAva)) {
-            out.println(expectAva);
-
-            Select Country = new Select(driver.findElementById("CountryId"));
-            Country.selectByVisibleText("United Kingdom");
-            Select Companyroll = new Select(driver.findElementById("CompanyRoleId"));
-            Companyroll.selectByVisibleText("Technical developer");
-            driver.findElement(By.className("custom-control-label")).click();
-            boolean newsLetter = driver.findElement(By.className("custom-control-label")).isSelected();
-            out.println(newsLetter);
-            Select Timezone = new Select(driver.findElementById("TimeZoneId"));
-            Timezone.selectByValue("GMT Standard Time");
-            driver.findElementById("Password").sendKeys("Password");
-            driver.findElementById("ConfirmPassword").sendKeys("Password");
-
-            WebElement Submit = driver.findElementById("register-button");
-            Submit.click();
-
-            Thread.sleep(1000);
-
-            String registration = driver.findElement(By.xpath("//div[@class='bar-notification success']/p")).getText();
-            if (registration.equals("Your registration completed")) out.println("Test Case Passed");
-        }
-        else out.println("Test Case fail");
-
         driver.quit();
 
     }
